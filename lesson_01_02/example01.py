d78665d6210889026c3b124dccaf8c809e898646
Janne Koponen <janne.koponen@savonia.fi>
2023-06-06 11:55:46 +0300

    new example

---
+++ lesson_01_02/example01.py
@@ -0,0 +1,8 @@
+#Author: Janne Koponen
+#Date:   6.6.2023
+#An arithmetic example
+
+print(5)
+print('2+3') #String
+print(2+3)   #Numeric value
+print('2+3 =', 2+3)
--- lesson_01_02/helloworld.py
+++ lesson_01_02/helloworld.py
@@ -1,6 +1,6 @@
 #Author: Janne Koponen
 #Date:   28.4.2023
 #
-#The 1st Python program. The software prints a message.
+#The software prints a message.
 
 print('Hello World!')
